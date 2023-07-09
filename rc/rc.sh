0344a06b19fc3b8f929136c6eaf723e7bf804753
filipditrich <ditrich@nfctron.com>
2023-07-09 23:04:28 +0200

    chore: add fast release candidate bash script
    Signed-off-by: filipditrich <ditrich@nfctron.com>

---
+++ rc/rc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Releasing main PDF..."
+cp ../dist/main.pdf ./BP_DITRICH_FILIP_2023.pdf
+echo "Releasing extended summary PDF..."
+cp ../dist-ext/extended-summary.pdf ./EXTENDED_SUMMARY_DITRICH_FILIP_2023.pdf
